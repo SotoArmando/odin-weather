8b105ecffd2c9b7b7ddb1dfbbac521379750dd2c
SotoArmando <armando29@live.com>
2019-10-21 14:32:36 -0400

    feature(firspreview): added map funcs

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+app.use(bodyParser.json())
 var unirest = require("unirest");
+const fetch = require('node-fetch');
 const path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/../dist/index.html'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
 
-app.get('/getweather', function (req, res) {
-    var req0 = unirest("GET", `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly?lang=eng&lat=${req.params.lat}&lon=${req.params.long}&hours=48`);
-
-    req0.headers({
-        "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-        "x-rapidapi-key": "352fae4cc4msh403045479e456c0p1c2ce7jsnc94ed5a5de5d"
-    });
+app.get('/getweather', function(req, res) {
+    // console.log(req.params.lat, req.params.long)
+    // console.log(req.query);
+    // console.log();
+    // console.log();
     
 
-    req0.end(function (res0) {
-        if (res0.error) {
-            res.send(res0.error);
-        };
-        
-        console.log(res0.body);
-        res.send(res0.body);
-    })
+    var url = `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lang=eng&lat=${req.query["lat"]}&lon=${req.query["long"]}&hours=48`;
+    var headers = {
+        "Content-Type": "application/json",
+        "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+        "x-rapidapi-key": "352fae4cc4msh403045479e456c0p1c2ce7jsnc94ed5a5de5d"
+    }
 
-  });
+    fetch(url, { method: 'GET', headers: headers })
+        .then((res) => {
+            return res.json()
+        })
+        .then((json) => {
+            
+            res.send(json);
+        });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
 });
 
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
