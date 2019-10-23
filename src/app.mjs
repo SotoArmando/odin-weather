2e96a2b0a8370b66e0f8432b79a8d8ff138c59a7
SotoArmando <armando29@live.com>
2019-10-23 14:39:25 -0400

    feature(firstpreview): added better scopes

--- src/app.mjs
+++ src/app.mjs
@@ -1,14 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const unirest = require("unirest");
-const fetch = require('node-fetch');
-const path = require('path');
 
-var app = express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let app = express();
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../dist/index.html'));
@@ -16,8 +17,8 @@ app.get('/', function(req, res) {
 
 app.get('/getweather', function(req, res) {
 
-    var url = `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lang=eng&lat=${req.query["lat"]}&lon=${req.query["long"]}&hours=48`;
-    var headers = {
+    let url = `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lang=eng&lat=${req.query["lat"]}&lon=${req.query["long"]}&hours=48`;
+    let headers = {
         "Content-Type": "application/json",
         "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
         "x-rapidapi-key": "352fae4cc4msh403045479e456c0p1c2ce7jsnc94ed5a5de5d"
@@ -38,5 +39,5 @@ app.get('/getweather', function(req, res) {
 });
 
 app.listen(3000, function() {
-    
+    console.log("Find me running at 127.0.0.1:3000")
 });
